ebfc8b547f589954975c9561520297f5e4ecde9b
SAIKO9X <cardosed3@gmail.com>
2024-07-28 09:45:16 -0300

    feat(emtity): atualiza entidade Restaurant com novas propriedades
    Adiciona relacionamentos com User, Address, Order, Food e propriedades adicionais como name, description, contact, cuisineType, openingHours, images, e registrationDate.

--- src/main/java/com/food/ordering/model/entities/Restaurant.java
+++ src/main/java/com/food/ordering/model/entities/Restaurant.java
@@ -1,12 +1,15 @@
 package com.food.ordering.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.food.ordering.model.dto.ContactInformation;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +20,37 @@ public class Restaurant {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  @OneToOne
+  private User owner;
+
+  @OneToOne
+  private Address address;
+
+  @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Order> orders = new ArrayList<>();
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
+  private List<Food> foods = new ArrayList<>();
+
+  private boolean open;
+  private String name;
+  private String description;
+
+  @Embedded
+  private ContactInformation contact;
+
+  @Column(name = "cuisine_type")
+  private String cuisineType;
+
+  @Column(name = "opening_hours")
+  private String openingHours;
+
+  @ElementCollection
+  @Column(length = 1000)
+  private List<String> images;
+
+  @Column(name = "registration_date")
+  private LocalDateTime registrationDate;
 }
