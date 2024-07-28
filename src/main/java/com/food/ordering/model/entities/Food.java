38aebe903147cfa084dea8fcbf9b762969e70cff
SAIKO9X <cardosed3@gmail.com>
2024-07-28 09:32:09 -0300

    feat(entity): atualiza entidade Food com novas propriedades
    Adiciona relacionamentos com Category, Restaurant, IngredientsItem e propriedades adicionais como name, description, price, availability, isVegetarian, isSeasonal, images, e creationDate.

--- src/main/java/com/food/ordering/model/entities/Food.java
+++ src/main/java/com/food/ordering/model/entities/Food.java
@@ -1,13 +1,15 @@
 package com.food.ordering.model.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jdk.jfr.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +20,33 @@ public class Food {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  @ManyToOne
+  @Column(name = "food_category")
+  private Category foodCategory;
+
+  @ManyToOne
+  private Restaurant restaurant;
+
+  @ManyToMany
+  private List<IngredientsItem> ingredients = new ArrayList<>();
+
+  private String name;
+  private String description;
+  private double price;
+  private boolean available;
+
+  @Column(name = "is_vegetarian")
+  private boolean isVegetarian;
+
+  @Column(name = "is_seasonal")
+  private boolean isSeasonal;
+
+  @Column(length = 1000)
+  @ElementCollection
+  private List<String> images;
+
+
+  @Column(name = "creation_date")
+  private Date creationDate;
 }
