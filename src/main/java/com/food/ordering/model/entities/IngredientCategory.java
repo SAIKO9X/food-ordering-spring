df3cd1dab3e0acea1255d9df9a8b4d6c8ba1ffcb
SAIKO9X <cardosed3@gmail.com>
2024-07-28 09:53:26 -0300

    feat(entities): atualiza entidade IngredientsItem e cria entidade IngredientCategory
    Atualiza IngredientsItem com novos relacionamentos e propriedades, e cria a nova entidade IngredientCategory com seus relacionamentos e propriedades.

--- src/main/java/com/food/ordering/model/entities/IngredientCategory.java
+++ src/main/java/com/food/ordering/model/entities/IngredientCategory.java
@@ -7,24 +7,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "tb_ingredients")
-public class IngredientsItem {
+@Entity(name = "tb_ingredient_category")
+public class IngredientCategory {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @ManyToOne
-  private IngredientCategory category;
-
-
   @JsonIgnore
   @ManyToOne
   private Restaurant restaurant;
 
+  @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+  private List<IngredientsItem> ingredients = new ArrayList<>();
+
   private String name;
-  private boolean stoke = true;
 }
