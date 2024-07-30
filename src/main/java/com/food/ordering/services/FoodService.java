e3660c59760b7177ef2147ed685a21832eab61a0
SAIKO9X <cardosed3@gmail.com>
2024-07-29 23:05:38 -0300

    feat(service): criar FoodService com métodos para gerenciamento de alimentos
    Define a interface FoodService com métodos para criar, deletar, buscar e gerenciar alimentos em restaurantes, incluindo filtragem por categoria e status de disponibilidade.

---
+++ src/main/java/com/food/ordering/services/FoodService.java
@@ -0,0 +1,29 @@
+package com.food.ordering.services;
+
+import com.food.ordering.model.entities.Category;
+import com.food.ordering.model.entities.Food;
+import com.food.ordering.model.entities.Restaurant;
+import com.food.ordering.model.request.CreateFoodRequest;
+
+import java.util.List;
+
+public interface FoodService {
+
+  public Food createFood(CreateFoodRequest request, Category category, Restaurant restaurant);
+
+  void deleteFood(Long foodId) throws Exception;
+
+  List<Food> getRestaurantsFood(
+    Long restaurantId,
+    boolean isVegetarian,
+    boolean noVegetarian,
+    boolean isSeasonal,
+    String foodCategory
+  );
+
+  public List<Food> searchFood(String keyword);
+
+  public Food findFoodById(Long foodId) throws Exception;
+
+  public Food updateAvailableStatus(Long foodId) throws Exception;
+}
