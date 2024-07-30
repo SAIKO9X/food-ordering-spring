3925585e6a1f0b509948fb15d124e910ac9c2b94
SAIKO9X <cardosed3@gmail.com>
2024-07-30 04:14:52 -0300

    feat(service): implementar IngredientsServiceImpl com métodos para gerenciamento de categorias e itens de ingredientes
    Implementa IngredientsServiceImpl com métodos para criar, buscar e atualizar categorias e itens de ingredientes. Integra repositórios IngredientsCategoryRepository e IngredientsItemRepository.

--- src/main/java/com/food/ordering/repositories/IngredientsCategoryRepository.java
+++ src/main/java/com/food/ordering/repositories/IngredientsCategoryRepository.java
@@ -3,5 +3,9 @@ package com.food.ordering.repositories;
 import com.food.ordering.model.entities.IngredientCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IngredientsCategoryRepository extends JpaRepository<IngredientCategory, Long> {
+
+  List<IngredientCategory> findByRestaurantId(Long id);
 }
